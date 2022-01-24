8b3c6a517fbb07674d4d345d9978e2f1a9e81f37
Victor Nova <lost@losttech.software>
2022-01-23 17:21:58 -0800

    updated copyright year

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Screen")]
-[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2021")]
+[assembly: AssemblyCopyright("Copyright © Lost Tech LLC 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
